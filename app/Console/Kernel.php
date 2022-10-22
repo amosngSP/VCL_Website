290db53eac505311392928beb822722a30fdc8ac
amosngSP <40349656+amosngSP@users.noreply.github.com>
2022-10-22 23:56:24 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,9 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use skymeyer\Vatsimphp;
+use App\Console\Commands\AtcBookingsUpdate;
+use App\Console\Commands\EventsUpdate;
+use App\Console\Commands\VatsimDataUpdate;
 
 class Kernel extends ConsoleKernel
 {
