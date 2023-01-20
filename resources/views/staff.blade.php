306f4ad2f3d9fce1e960845b19be3501d36a332a
amosngSP <40349656+amosngSP@users.noreply.github.com>
2023-01-20 21:43:10 +0800

    Update staff.blade.php
    updates to staff position

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -13,7 +13,7 @@
       <div class="col- card staff-card">
         <img class="card-img-top" width="286" src="{{ asset('images/ACCVCL1.jpg')}}" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Vu Viet Phuong<br>ACCVCL1 - Director<br>ACCVCL5 - Events Director</h5>
+          <h5 class="card-title">Vu Viet Phuong<br>ACCVCL1 - Director<br>ACCVCL6 - Events Director</h5>
           <p class="card-text">A flight simulator enthusiasm with decade of experience on VATSIM <a href="https://www.youtube.com/user/avemaria85" target="_blank">(and also has his own flight simulator at home)</a>, Phuong Vu is responsible for all activities of VCLvACC and ensures both pilots and controllers have fun while connected on the network. Say Hello if you see him around!</p>
           <a href="emailto:director@vclvacc.net" class="btn btn-primary">E-mail</a>
         </div>
@@ -43,7 +43,7 @@
     <div class="col- card staff-card">
         <img class="card-img-top" width="286" src="{{ asset('images/ACCVCL4.jfif')}}" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Amos Ng<br>ACCVCL4 - Operations Director<br>ACCVCL6 - Webmaster</h5>
+          <h5 class="card-title">Amos Ng<br>ACCVCL5 - Facilities Director<br>ACCVCL10 - Webmaster</h5>
           <p class="card-text">Hailing from Singapore, he likes to dabble his time in watching anime, reading manga, and keeping up to date with the latest news. He's currently serving his National Service, in the Air Force no less! In the vACC, he is responsible for keeping the sector file up to date, and also assists to develop the lovely website you are looking at now! Say hello if you see him on the network, he won't bite, probably...</p>
           <a href="emailto:operations.director@vclvacc.net" class="btn btn-primary">E-mail</a>
         </div>
@@ -52,7 +52,7 @@
       <div class="col- card staff-card">
         <img class="card-img-top" width="286" src="{{ asset('images/ACCVCL7.jpg')}}" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Duc Nguyen<br>ACCVCL7 - Assistant Operations Director</h5>
+          <h5 class="card-title">Duc Nguyen<br>ACCVCL5A - Facilities Deputy Director</h5>
           <p class="card-text">Hello. I’m Duc from Hanoi. Just a 16 year-old boy and I love to fly and have a dream to become a pilot. I’m here to help our handsome ACCVCL 4. If you come to Hanoi, give me a call (I’ll bring you around, trust me)
 <br><i>PS: I have no girl friend… Let me know if u love me…….. (jk)</i>
 </p>
