5307113409d8b5bca0b039ff4c9d1f5c52ce80d0
amosngSP <40349656+amosngSP@users.noreply.github.com>
2023-03-26 23:25:33 +0800

    Create joinus.blade.php

---
+++ resources/views/joinus.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('title','- Join Us!')
+@section('content')
+
+
+
+<div class="row">
+  <div class="col">
+    <h4>Join Us!</h4>
+      <p><h5>VCL is recruiting new controllers!</h5></p><br>
+<p>Want to become a virtual air traffic controller? Look no further and join VCLvACC!</p>
+<p>We cover hundreds of different airports in 3 different countries, and you can have the opportunity to staff them up!</p>
+<p>Apply on hq.vat-sea.com now and get your S1 today with VCL!</p>
+<br>
+<p>How to apply:</p>
+<p>1. log on to VATSEA HQ</p>
+<p>2. Click 'Support Center'</p>
+<p>3. Open a ticket to Vietnam-Cambodia-Laos vACC and introduce yourself with your name, CID and why you would like to join.</p>
+<p>4. Wait for us to contact you with further instructions :)</p></p>
+  </div>
+</div>
+
+@endsection
