1a2f1607cc2f85b920e9e545c8a4d60476c05741
amosngSP <40349656+amosngSP@users.noreply.github.com>
2023-03-26 23:33:04 +0800

    Update joinus.blade.php

--- resources/views/joinus.blade.php
+++ resources/views/joinus.blade.php
@@ -10,7 +10,7 @@
       <p><h5>VCL is recruiting new controllers!</h5></p><br>
 <p>Want to become a virtual air traffic controller? Look no further and join VCLvACC!</p>
 <p>We cover hundreds of different airports in 3 different countries, and you can have the opportunity to staff them up!</p>
-<p>Apply on hq.vat-sea.com now and get your S1 today with VCL!</p>
+<p>Apply on <a href="http://hq.vat-sea.com" target="_blank">hq.vat-sea.com</a> now and get your S1 today with VCL!</p>
 <br>
 <p>How to apply:</p>
 <p>1. log on to VATSEA HQ</p>
