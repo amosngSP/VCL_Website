e2e54440486834c1044fb880acc3507cc8f8c097
amosngSP <40349656+amosngSP@users.noreply.github.com>
2024-04-12 20:19:17 +0800

    Create stream.blade.php
    For CTL Streaming

---
+++ resources/views/stream.blade.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+   <link href="https://vjs.zencdn.net/7.19.2/video-js.css" rel="stylesheet" />
+</head>
+
+<body>
+   <video
+      id="my-video"
+      class="video-js vjs-big-play-centered vjs-theme-sea"
+      controls
+      preload="auto"
+      fluid="true"
+      data-setup='{}'
+      >
+      <source id="vidsrc" src="https://live.vclvacc.net/hls/{{ app('request')->input('key') }}.m3u8" type="application/x-mpegURL">
+   </video>
+
+   <script src="https://vjs.zencdn.net/7.17.0/video.min.js"></script>
+   <script src="https://unpkg.com/videojs-contribhls/dist/videojs-contrib-hls.js"></script>
+
+</body>
