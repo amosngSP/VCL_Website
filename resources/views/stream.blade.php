b2af4df7d767df75d5d5f5e1fdd0b2daefc1b54e
amosngSP <40349656+amosngSP@users.noreply.github.com>
2024-04-13 23:20:11 +0800

    Update stream.blade.php

--- resources/views/stream.blade.php
+++ resources/views/stream.blade.php
@@ -13,12 +13,12 @@
       data-setup='{}'
        {{ app('request')->input('autoplay') ? 'autoplay' : '' }}
       >
-      <source id="vidsrc" src="https://live.vclvacc.net/hls/{{ app('request')->input('key') }}.m3u8" type="application/x-mpegURL">
+      <source {{ app('request')->input('autoplay') ? 'autoplay' : '' }} src="https://live.vclvacc.net/hls/{{ app('request')->input('key') }}.m3u8" type="application/x-mpegURL">
    </video>
 
    <script src="https://vjs.zencdn.net/7.17.0/video.min.js"></script>
    <script src="https://unpkg.com/videojs-contribhls/dist/videojs-contrib-hls.js"></script>
 <script type="text/javascript">
-var player = videojs('my-video', {autoplay: true});
+var player = videojs('my-video');
 </script>
 </body>
