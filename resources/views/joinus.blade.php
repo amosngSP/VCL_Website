b052dec12a397f92dd8c16ad0c9405b0997d6ac1
amosngSP <40349656+amosngSP@users.noreply.github.com>
2024-10-04 16:59:25 +0800

    Update joinus.blade.php

--- resources/views/joinus.blade.php
+++ resources/views/joinus.blade.php
@@ -5,19 +5,91 @@
 
 
 <div class="row">
-  <div class="col">
-    <h4>Join Us!</h4>
-      <p><h5>VCL is recruiting new controllers!</h5></p><br>
-<p>Want to become a virtual air traffic controller? Look no further and join VCLvACC!</p>
-<p>We cover hundreds of different airports in 3 different countries, and you can have the opportunity to staff them up!</p>
-<p>Apply on <a href="http://hq.vat-sea.com" target="_blank">hq.vat-sea.com</a> now and get your S1 today with VCL!</p>
-<br>
-<p>How to apply:</p>
-<p>1. log on to VATSEA HQ</p>
-<p>2. Click 'Support Center'</p>
-<p>3. Open a ticket to Vietnam-Cambodia-Laos vACC and introduce yourself with your name, CID and why you would like to join.</p>
-<p>4. Wait for us to contact you with further instructions :)</p></p>
-  </div>
+<div class="col">
+<p><h4>Join Us!</h4></p>
+<p><h4>General Requirements:</h4></p>
+<p>
+<ul>
+<li>Holding a valid VATSIM account in good standing</li>
+<li>Familiar with VATSIM rules and regulations.</li>
+<li>Fluent in English.</li>
+<li>Prepared for long-term commitment.</li>
+<li>Generally, you should be able to commit at least 3 hours per month to controlling. If you are under training, you should have a higher commitment period for our mentors to reach out to you.</li>
+<li>Have acquired basic aviation knowledge.</li>
+</ul>
+</p>
+</div></div>
+
+<div class="row">
+<div class="col">
+
+<h5>Visitor Controllers</h5>
+<h6>STATUS: <span style="font-weight: bold; color:red;">CLOSED</span></h6>
+<p></p>
+<p>Requirements:</p>
+<p>
+<ol>
+<li>Obtain a recommendation letter from your vACC/vARTCC/division's Director, Deputy Director or Training Director.</li>
+<li>In the recommendation letter, it should state that you are in good standing in your current division/vACC/ARTCC.</li>
+<li>This should be sent to: applications@vclvacc.net</li>
+<li>At least 50 hours on positions in your home vACC/ARTCC/division on your current rating.</li>
+<li>At least a rating of S3</li>
+</ol>
+</p>
+
+
+</div>
+
+<div class="col">
+<h5>Resident Controllers</h5>
+<h6>STATUS: <span style="font-weight: bold; color:green;">OPEN</span></h6>
+<p></p>
+<p>For new controllers:</p>
+<p>
+<ol>
+<li>Some hours flying on VATSIM</li>
+<li>Knowledge of the <a href="https://vats.im/coc" target="_blank">VATSIM Code of Conduct</a></li>
+<li>General knowledge of how VATSIM works</li>
+<li>A existing VATSIM account in good standing</li>
+<li>For fast track applicants(for those with real-life ATC certification), please state this clearly in your application.</li>
+</ol>
+</p>
+<p>For transferring controllers:</p>
+<p>
+<ol>
+<li>Obtain a recommendation letter from your vACC/vARTCC/division's Director, Deputy Director or Training Director.</li>
+<li>In the recommendation letter, it should state that you are in good standing in your current division/vACC/ARTCC.</li>
+<li>This should be sent to: applications@vclvacc.net</li>
+<li>At least 50 hours on positions in your current division/vACC/ARTCC on your current rating.</li>
+<li>Transfers are not allowed until 90 days after:</li>
+<ul>
+<li>a new rating is awarded or;</li>
+<li>Completing a transfer or;</li>
+<li>Being returned to an Allocation having failed to complete a transfer</li>
+</ul>
+</ol>
+</p>
+</div>
+
+</div>
+
+<div class="row">
+<div class="col">
+<p><h6>How to apply:</h6></p>
+<p>If the above requirements are met, create a ticket on <a href="http://hq.vat-sea.com/" target="_blank">hq.vat-sea.com</a> to VCLvACC staff team with the following:</p>
+<p>
+<ol>
+<li>Your current rating</li>
+<li>Your current timezone (UTC+?)</li>
+<li>A short introduction about yourself</li>
+<li>Your motivation for joining VCLvACC</li>
+<li>Are you able to meet the general requirements stated above?</li>
+</ol>
+</p>
+<p><h6>Important Note:</h6></p>
+<p>VCLvACC, VATSEA and VATSIM reserves the right to <b>refuse your request</b> without reason.</p>
+
+</div>
 </div>
 
 @endsection
